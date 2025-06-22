9371be3b4d1079ce90bdb39a3323ea23f3da7329
kamiy030 <2024530795@student.uitm.edu.my>
2025-06-23 02:32:55 +0800

    Update aco_app.py

--- aco_app.py
+++ aco_app.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import pydeck as pdk
 
+# Set Mapbox token (REQUIRED for map styles like streets/satellite/outdoors)
+pdk.settings.mapbox_api_key = "pk.eyJ1Ijoia2FtaXkiLCJhIjoiY21jN3p3bGkxMHF5dTJtbjVnZWRoZ21ybCJ9.a_uMfg1RsYTuXh5N5C3AXA" 
+
 st.set_page_config(layout="wide")
 st.title("📍 Campus Navigation Optimizer using ACO")
 
@@ -128,10 +131,7 @@ if uploaded_file and coord_file:
             get_color=[0, 0, 0],
             get_size=16,
             get_alignment_baseline="'bottom'"
-        )
-
-# Set Mapbox token (REQUIRED for map styles like streets/satellite/outdoors)
-pdk.settings.mapbox_api_key = "pk.eyJ1Ijoia2FtaXkiLCJhIjoiY21jN3p3bGkxMHF5dTJtbjVnZWRoZ21ybCJ9.a_uMfg1RsYTuXh5N5C3AXA-token"  
+        ) 
         
         mid_lat, mid_lon = path_coords[0]
         st.pydeck_chart(pdk.Deck(
